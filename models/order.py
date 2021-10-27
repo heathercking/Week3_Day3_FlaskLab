a236158bdf1aa62265f7e18cfa8f627a8542e7bc
Sam <samuel.fitz@btinternet.com>
2021-10-27 14:43:13 +0100

    MVP Complete
    Added everything

---
+++ models/order.py
@@ -0,0 +1,8 @@
+class Order():
+    def __init__(self, customer_name, order_number, order_date, quantity, cake):
+        self.customer_name = customer_name
+        self.order_number = order_number
+        self.order_date = order_date
+        self.quantity = quantity
+        self.cake = cake
+        
---
+++ models/order_list.py
@@ -0,0 +1,7 @@
+from models.order import *
+
+order_1 = Order("JP", "01", "10/27", 10, "Halloween Cake")
+order_2 = Order("Heather", "02", "1/14", 1, "Birthday Cake")
+order_3 = Order("Sam", "03", "12/23", 2, "Christmas Cake")
+
+orders = [order_1, order_2, order_3]
