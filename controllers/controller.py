b8aeb9d1e5ec331f4819d88c8cebd0a4c7945e6f
Sam <samuel.fitz@btinternet.com>
2021-10-27 15:39:12 +0100

    Tried to add source

--- controllers/controller.py
+++ controllers/controller.py
@@ -9,3 +9,15 @@ def index():
 @app.route('/orders/<index>')
 def order(index):
     return render_template('order.html', order = orders[int(index)-1])
+
+@app.route('/orders/1')
+def order_1():
+    return render_template('order.html', order = orders[0])
+
+@app.route('/orders/2')
+def order_2():
+    return render_template('order.html', order = orders[1])
+
+@app.route('/orders/3')
+def order_3():
+    return render_template('order.html', order = orders[2])
